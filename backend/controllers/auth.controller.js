38e85c79f97d7bbae1b6aeec544b390906a65f04
Mustafa Nalbant <90528752+mustafanalbant1@users.noreply.github.com>
2025-02-22 16:58:07 +0300

    1-Project Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "You hit the signup endpoit",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    data: "You hit the login endpoit",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    data: "You hit the logout endpoit",
+  });
+};
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use(cors());
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+  connectMongoDB();
 });
